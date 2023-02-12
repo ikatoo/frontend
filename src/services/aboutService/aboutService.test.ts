1a81d56c2bc9d679819fabf4f8dcc301f8c23eeb
Milton Carlos Katoo <mckatoo@gmail.com>
2023-02-12 17:38:13 -0300

    🤡 (test): mocked methods from axios

--- src/services/aboutService/aboutService.test.ts
+++ src/services/aboutService/aboutService.test.ts
@@ -1,17 +1,62 @@
 import { describe, test, vi } from 'vitest'
 
-// import aboutService from '.'
+import aboutService from '.'
 import { aboutPageMock } from '../../mocks/aboutPageMock'
-// import axios from 'axios'
+import api from '../api'
 
-vi.mock('axios')
+vi.mock('../api')
+
+api.get = vi.fn().mockResolvedValue({
+  data: aboutPageMock
+})
+
+api.post = vi.fn()
+api.put = vi.fn()
+api.delete = vi.fn()
+
+const headers = {
+  headers: {
+    Authorization: `bearer `,
+    ContentType: 'application/json'
+  }
+}
+
+const postAndPutArgs = [
+  '/about',
+  {
+    title: aboutPageMock.title,
+    description: aboutPageMock.description
+  },
+  headers
+]
 
 describe('About page fetch data', () => {
   test('should get about page data', async () => {
-    // axios.get = vi.fn().mockResolvedValue(aboutPageMock)
-    // const result = await aboutService.get()
+    const result = await aboutService.get()
 
-    // expect(result).toEqual(aboutPageMock)
-    expect(aboutPageMock).toEqual(aboutPageMock)
+    expect(api.get).toBeCalledTimes(1)
+    expect(api.get).toBeCalledWith('/about')
+    expect(result).toEqual(aboutPageMock)
+  })
+
+  test('should create about page data without errors', async () => {
+    await aboutService.create(aboutPageMock)
+
+    expect(api.post).toBeCalledTimes(1)
+    expect(api.post).toBeCalledWith(...postAndPutArgs)
+  })
+
+  test('should update about page data without errors', async () => {
+    await aboutService.update(aboutPageMock)
+
+    expect(api.put).toBeCalledTimes(1)
+    expect(api.put).toBeCalledWith(...postAndPutArgs)
+  })
+
+  test('should delete about page data without errors', async () => {
+    await aboutService.delete()
+
+    expect(api.delete).toBeCalledTimes(1)
+    expect(api.delete).toBeCalledWith('/about')
   })
 })
