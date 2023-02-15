f2cb78d803e6b8b7da2bfd74a14c009572582609
Milton Carlos Katoo <mckatoo@gmail.com>
2023-02-15 19:14:09 -0300

    update about page admin

--- src/pages/Admin/AdminAbout/index.tsx
+++ src/pages/Admin/AdminAbout/index.tsx
@@ -5,54 +5,59 @@ import TextEditor from '../../../components/TextEditor'
 import { FormContainer } from '../../../components/FormContainer'
 import { TextContainer } from '../../../components/TextContainer'
 import TextInput from '../../../components/TextInput'
-// import { useAlert } from '../../../hooks/useAlert'
-// import aboutService from '../../../services/aboutService'
+import { useAlert } from '../../../hooks/useAlert'
+import aboutService from '../../../services/aboutService'
 import Styles from './styles'
+import TagInput from '../../../components/TagInput'
 
 export const AdminAbout = () => {
-  const auth = { user: { id: '' } }
-  // const { setAlert } = useAlert()
+  const { setAlert } = useAlert()
 
-  // const [id, setId] = useState<string>()
-  const [title, setTitle] = useState<string>()
-  const [description, setDescription] = useState<string>()
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [skills, setSkills] = useState<string[]>([])
+  const [illustrationURL, setIllustrationURL] = useState('')
+  const [illustrationALT, setIllustrationALT] = useState('')
 
   useEffect(() => {
-    // const getInitialData = async () => {
-    //   if (auth.user?.id) {
-    //     const initialData = await aboutService.get(auth.user?.id ?? '')
-    //     if (initialData) {
-    //       setId(initialData.id)
-    //       setTitle(initialData.title)
-    //       setDescription(initialData.description)
-    //     }
-    //   }
-    // }
-    // getInitialData()
-  }, [auth.user?.id])
+    const getInitialData = async () => {
+      const initialData = await aboutService.get()
+      if (initialData) {
+        setTitle(initialData.title)
+        setDescription(initialData.description)
+        setSkills(initialData.skills.map((skill) => skill.title))
+        setIllustrationURL(initialData.illustrationURL)
+        setIllustrationALT(initialData.illustrationALT)
+      }
+    }
+    getInitialData()
+  }, [])
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
 
-    // if (!id) {
-    //   aboutService.create({
-    //     title: title,
-    //     description: description,
-    //     user_id: auth.user?.id
-    //   })
-    //   setAlert({
-    //     title: 'Success on create about page.',
-    //     type: 'message'
-    //   })
-    // } else {
-    //   aboutService.update({
-    //     id: id,
-    //     title: title,
-    //     description: description,
-    //     user_id: auth.user?.id
-    //   })
-    //   setAlert({ title: 'Success on update about page.', type: 'message' })
-    // }
+    if (!title.length) {
+      aboutService.create({
+        title,
+        description,
+        skills: skills.map((skill) => ({ title: skill })),
+        illustrationURL,
+        illustrationALT
+      })
+      setAlert({
+        title: 'Success on create about page.',
+        type: 'message'
+      })
+    } else {
+      aboutService.update({
+        title,
+        description,
+        skills: skills.map((skill) => ({ title: skill })),
+        illustrationURL,
+        illustrationALT
+      })
+      setAlert({ title: 'Success on update about page.', type: 'message' })
+    }
   }
 
   return (
@@ -85,8 +90,42 @@ export const AdminAbout = () => {
               />
             </Styles.TextWrapper>
 
+            <TagInput
+              initialValue={skills}
+              label="Skills"
+              labelColor="white"
+              name="skills"
+              placeholder="Skills"
+              onBlur={(value) =>
+                setSkills([...skills, value.currentTarget.value])
+              }
+              tabIndex={3}
+            />
+
+            <TextInput
+              initialValue={illustrationURL}
+              labelColor="white"
+              label="Illustration URL"
+              name="illustrationURL"
+              placeholder="http://urltoyourillustration"
+              onBlur={(event) => setIllustrationURL(event.currentTarget.value)}
+              tabIndex={4}
+              autoFocus
+            />
+
+            <TextInput
+              initialValue={illustrationALT}
+              labelColor="white"
+              label="Illustration ALT"
+              name="illustrationALT"
+              placeholder="Brief description of the illustration."
+              onBlur={(event) => setIllustrationALT(event.currentTarget.value)}
+              tabIndex={5}
+              autoFocus
+            />
+
             <Styles.Actions>
-              <Button tabIndex={3} styleType="primary">
+              <Button tabIndex={6} styleType="primary">
                 Salvar
               </Button>
             </Styles.Actions>
--- src/pages/Admin/AdminAbout/stories.tsx
+++ src/pages/Admin/AdminAbout/stories.tsx
@@ -1,10 +1,11 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Meta, StoryFn } from '@storybook/react'
 import { AdminAbout } from '.'
 
 export default {
   title: 'Pages/Privates/AdminAbout',
   component: AdminAbout,
   decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>]
-} as ComponentMeta<typeof AdminAbout>
+} as Meta<typeof AdminAbout>
 
-export const Default = {} as ComponentStory<typeof AdminAbout>
+export const Default = {} as StoryFn<typeof AdminAbout>
+export const WithInitiallData = {} as StoryFn<typeof AdminAbout>
