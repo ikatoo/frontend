f484c3d690e81873398d0542f3aabdfb169af7a9
Milton Carlos Katoo <mckatoo@gmail.com>
2023-02-21 12:20:11 -0300

    fix mock data

--- src/pages/About/stories.tsx
+++ src/pages/About/stories.tsx
@@ -1,9 +1,24 @@
 import { Meta, StoryFn } from '@storybook/react'
+import { rest } from 'msw'
+
 import { About } from '.'
+import env from '../../helpers/env'
+import { aboutPageMock } from '../../mocks/aboutPageMock'
+import { aboutPageRequestHandlers } from '../../mocks/handlers/about'
 
 export default {
   title: 'Pages/About',
   component: About,
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(`${env.VITE_API_URL}/about`, (_req, res, ctx) => {
+          return res(ctx.status(200), ctx.json(aboutPageMock))
+        }),
+        ...aboutPageRequestHandlers
+      ]
+    }
+  },
   decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>]
 } as Meta<typeof About>
 
--- src/pages/Admin/AdminAbout/stories.tsx
+++ src/pages/Admin/AdminAbout/stories.tsx
@@ -5,21 +5,9 @@ import { AdminAbout } from '.'
 import { aboutPageMock } from '../../../mocks/aboutPageMock'
 import { AlertProvider } from '../../../hooks/useAlert'
 import env from '../../../helpers/env'
+import { aboutPageRequestHandlers } from '../../../mocks/handlers/about'
 
-const handlers = [
-  rest.get(`${env.VITE_API_URL}/about`, (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({}))
-  }),
-  rest.post(`${env.VITE_API_URL}/about`, (_req, res, ctx) => {
-    return res(ctx.status(201))
-  }),
-  rest.put(`${env.VITE_API_URL}/about`, (_req, res, ctx) => {
-    return res(ctx.status(204))
-  }),
-  rest.delete(`${env.VITE_API_URL}/about`, (_req, res, ctx) => {
-    return res(ctx.status(204))
-  })
-]
+const handlers = aboutPageRequestHandlers
 
 export default {
   title: 'Pages/Privates/AdminAbout',
