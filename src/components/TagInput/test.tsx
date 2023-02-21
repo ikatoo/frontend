7b07e8d02a35da3ad82142fe99980cba8dc9d436
Milton Carlos Katoo <mckatoo@gmail.com>
2023-02-20 21:39:58 -0300

    ♻️ (refactor): pass function property for change tags

--- src/components/TagInput/test.tsx
+++ src/components/TagInput/test.tsx
@@ -1,33 +1,72 @@
+import { act, render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { vi } from 'vitest'
 
 import TagInput from '.'
-import { act, render, screen, waitFor } from '@testing-library/react'
-import { vi } from 'vitest'
-import theme from '../../theme'
 
 describe('<TagInput />', () => {
   it('Renders with Label', () => {
-    render(<TagInput label="Label" name="Label" />)
+    let skills: string[] = []
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
+    render(
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        label="Label"
+        name="Label"
+      />
+    )
 
     expect(screen.getByLabelText('Label')).toBeInTheDocument()
   })
 
   it('Renders without Label', () => {
-    render(<TagInput name="username" />)
+    let skills: string[] = []
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
+    render(
+      <TagInput onTagsChange={setSkills} initialTags={skills} name="username" />
+    )
 
     expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
   })
 
   it('Renders with placeholder', () => {
-    render(<TagInput name="username" placeholder="hey you" />)
+    let skills: string[] = []
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
+    render(
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        name="username"
+        placeholder="hey you"
+      />
+    )
 
     expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument()
   })
 
   it('Changes its value when typing', async () => {
-    const onInputChange = vi.fn()
+    let skills: string[] = []
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
     render(
-      <TagInput onInputChange={onInputChange} label="Skills" name="skills" />
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        label="Skills"
+        name="skills"
+      />
     )
 
     const input = screen.getByRole('textbox')
@@ -39,16 +78,16 @@ describe('<TagInput />', () => {
 
     await waitFor(() => {
       expect(input).toHaveValue(text)
-      expect(onInputChange).toHaveBeenCalledTimes(text.length)
     })
-    expect(onInputChange).toHaveBeenCalledWith(text)
   })
 
   it('Does not changes its value when disabled', async () => {
-    const onInputChange = vi.fn()
+    const setSkills = vi.fn()
+
     render(
       <TagInput
-        onInputChange={onInputChange}
+        onTagsChange={setSkills}
+        initialTags={[]}
         label="TagInput"
         name="TagInput"
         disabled
@@ -60,19 +99,28 @@ describe('<TagInput />', () => {
 
     const text = 'This is my new text'
 
-    act(() => {
-      userEvent.type(input, text)
-    })
+    userEvent.type(input, text)
 
     await waitFor(() => {
       expect(input).not.toHaveValue(text)
     })
-    expect(onInputChange).not.toHaveBeenCalled()
+    expect(setSkills).not.toHaveBeenCalled()
   })
 
   it('Renders with error', () => {
+    let skills: string[] = []
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
     const { container } = render(
-      <TagInput name="username" label="TagInput" error="Error message" />
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        name="username"
+        label="TagInput"
+        error="Error message"
+      />
     )
 
     expect(screen.getByText('Error message')).toBeInTheDocument()
@@ -81,7 +129,19 @@ describe('<TagInput />', () => {
   })
 
   it('Is accessible by tab', () => {
-    render(<TagInput label="TagInput" name="TagInput" />)
+    let skills: string[] = []
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
+    render(
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        label="TagInput"
+        name="TagInput"
+      />
+    )
 
     const input = screen.getByLabelText('TagInput')
     expect(document.body).toHaveFocus()
@@ -93,72 +153,120 @@ describe('<TagInput />', () => {
     expect(input).toHaveFocus()
   })
 
-  it('Is not accessible by tab when disabled', () => {
-    render(<TagInput label="TagInput" name="TagInput" disabled />)
+  it('Is not accessible by tab when disabled', async () => {
+    let skills: string[] = []
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
+    render(
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        label="TagInput"
+        name="TagInput"
+        disabled
+      />
+    )
 
     const input = screen.getByLabelText('TagInput')
     expect(document.body).toHaveFocus()
 
-    act(() => {
-      userEvent.tab()
-    })
+    userEvent.tab()
 
-    expect(input).not.toHaveFocus()
+    await waitFor(() => {
+      expect(input).not.toHaveFocus()
+    })
   })
 
-  it('When typing comma circul the text with gray background up to previous comma', () => {
-    render(<TagInput label="TagInput" name="TagInput" />)
+  it('When typing comma, add item to skills array', async () => {
+    let skills: string[] = []
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
+    render(
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        label="TagInput"
+        name="TagInput"
+      />
+    )
     const input = screen.getByLabelText('TagInput')
     const text = 'skill typed'
 
+    expect(skills).toHaveLength(0)
+
     userEvent.type(input, `${text},`)
 
-    const tag = screen.getByText(text)
-
-    expect(tag).toBeInTheDocument()
-    expect(tag).toHaveStyle(`
-      background-color: ${theme.colors.gray};
-      color: ${theme.colors.lightGray};
-    `)
+    await waitFor(() => {
+      expect(skills).toHaveLength(1)
+    })
   })
 
-  it('When typing backspace and input is empty, remove the last tag', () => {
-    render(<TagInput label="TagInput" name="TagInput" />)
-    const input = screen.getByLabelText('TagInput')
-    const lastTag = 'last skill'
-
-    userEvent.type(input, 'skill 1,')
-    userEvent.type(input, 'skill 2,')
-    userEvent.type(input, `${lastTag},`)
+  it('Render with initial tags', async () => {
+    render(
+      <TagInput
+        onTagsChange={(tags: string[]) => tags}
+        initialTags={['skill1', 'skill2', 'skill3']}
+        label="TagInput"
+        name="TagInput"
+      />
+    )
 
     const tags = screen.getAllByTestId('tag-test-id')
+
     expect(tags).toHaveLength(3)
+  })
+
+  it('When typing backspace and input is empty, remove the last tag', async () => {
+    let skills: string[] = ['skill1', 'skill2', 'skill3']
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
+
+    render(
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        label="TagInput"
+        name="TagInput"
+      />
+    )
+    const input = screen.getByLabelText('TagInput')
+
+    const tagsBefore = screen.getAllByTestId('tag-test-id')
+    expect(tagsBefore).toHaveLength(3)
 
     userEvent.type(input, '{backspace}')
 
-    const tagsBefore = screen.getAllByTestId('tag-test-id')
-    expect(tagsBefore).toHaveLength(2)
+    await waitFor(() => {
+      expect(skills).toStrictEqual(['skill1', 'skill2'])
+    })
   })
 
-  it('Click for remove a tag', () => {
-    render(<TagInput label="TagInput" name="TagInput" />)
-    const input = screen.getByLabelText('TagInput')
-    const tagForRemove = 'remove skill'
+  it('Click for remove a tag', async () => {
+    let skills: string[] = ['skill1', 'skill2', 'skill3']
+    const setSkills = (tags: string[]) => {
+      skills = tags
+    }
 
-    userEvent.type(input, 'skill 1,')
-    userEvent.type(input, `${tagForRemove},`)
-    userEvent.type(input, 'skill 3,')
+    render(
+      <TagInput
+        onTagsChange={setSkills}
+        initialTags={skills}
+        label="TagInput"
+        name="TagInput"
+      />
+    )
 
-    const tags = screen.getAllByTestId('tag-test-id')
-    expect(tags).toHaveLength(3)
+    const tagForRemove = screen.getByText('skill2')
+    tagForRemove.lastElementChild &&
+      userEvent.click(tagForRemove.lastElementChild)
 
-    const tagElementForRemove = screen.getByText(tagForRemove)
-    const closeElement = tagElementForRemove.querySelector('svg')
-
-    !!closeElement && userEvent.click(closeElement)
-
-    const tagsBefore = screen.getAllByTestId('tag-test-id')
-    expect(tagsBefore).toHaveLength(2)
-    expect(screen.queryByText(tagForRemove)).not.toBeInTheDocument()
+    await waitFor(() => {
+      expect(skills).toStrictEqual(['skill1', 'skill3'])
+    })
   })
 })
