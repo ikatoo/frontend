724e3d2174e97ab2b131ca13ac4702ee37bf15e8
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-04 14:21:52 -0300

    add memory router on context

--- src/pages/NotFound/test.tsx
+++ src/pages/NotFound/test.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import { describe, expect, it } from 'vitest'
 import { NotFound } from '.'
 
 describe('Not Found Page', () => {
   it('should render not found page when try access a bad route', () => {
-    const { getByText } = render(<NotFound />)
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <NotFound />
+      </MemoryRouter>
+    )
 
     expect(getByText(/page not found/i)).toBeInTheDocument()
   })
