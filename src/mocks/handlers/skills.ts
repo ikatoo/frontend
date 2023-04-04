71ff9ce580a869936916f37cc098d005b8e3bd29
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-04 11:03:02 -0300

    change route skill to skills

--- src/mocks/handlers/skills.ts
+++ src/mocks/handlers/skills.ts
@@ -3,16 +3,16 @@ import env from '../../helpers/env'
 import skillsPageMock from '../skillsPageMock'
 
 export default [
-  rest.get(`${env.VITE_API_URL}/skill`, (_req, res, ctx) => {
+  rest.get(`${env.VITE_API_URL}/skills`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(skillsPageMock))
   }),
-  rest.post(`${env.VITE_API_URL}/skill`, (_req, res, ctx) => {
+  rest.post(`${env.VITE_API_URL}/skills`, (_req, res, ctx) => {
     return res(ctx.status(201))
   }),
-  rest.patch(`${env.VITE_API_URL}/skill`, (_req, res, ctx) => {
+  rest.patch(`${env.VITE_API_URL}/skills`, (_req, res, ctx) => {
     return res(ctx.status(204))
   }),
-  rest.delete(`${env.VITE_API_URL}/skill`, (_req, res, ctx) => {
+  rest.delete(`${env.VITE_API_URL}/skills`, (_req, res, ctx) => {
     return res(ctx.status(204))
   })
 ]
