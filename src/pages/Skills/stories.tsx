69dad7953f24ceb51632d80ab70bba8d0480ca92
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-05 18:12:49 -0300

    add msw in stories

--- src/pages/Skills/stories.tsx
+++ src/pages/Skills/stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { Skills } from '.'
+import skills from '../../mocks/handlers/skills'
 
 export default {
   title: 'Pages/Skills',
@@ -8,7 +9,12 @@ export default {
     (Story) => (
       <div className="bg-mck_black_light text-mck_gray_light">{Story()}</div>
     )
-  ]
+  ],
+  parameters: {
+    msw: {
+      handlers: skills
+    }
+  }
 } as ComponentMeta<typeof Skills>
 
 export const Default = {} as ComponentStory<typeof Skills>
