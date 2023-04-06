7adddc9cf0e492500de6e1859a6d0b7424e3b916
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-06 11:57:37 -0300

    mock fetch about page api

--- src/pages/About/stories.tsx
+++ src/pages/About/stories.tsx
@@ -1,10 +1,16 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { About } from '.'
+import about from '../../mocks/handlers/about'
 
 export default {
   title: 'Pages/About',
   component: About,
-  decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>]
+  decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>],
+  parameters: {
+    msw: {
+      handlers: about
+    }
+  }
 } as ComponentMeta<typeof About>
 
 export const Default = {} as ComponentStory<typeof About>
