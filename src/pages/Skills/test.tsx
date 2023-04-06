11ecf5e7a194bb345c4535b38176b52226ad9160
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-06 17:03:57 -0300

    remove unnecessary mock

--- src/pages/Skills/test.tsx
+++ src/pages/Skills/test.tsx
@@ -1,11 +1,10 @@
 import { render, waitFor } from '@testing-library/react'
-import { describe, vi } from 'vitest'
+import { describe } from 'vitest'
 import { Skills } from '.'
 import skillsPageMock from '../../mocks/skillsPageMock'
-vi.mock('../../components/IconCloud')
 
 describe('Skills Page', () => {
-  test('renders the about page with data from the server', async () => {
+  test('renders the skill page with data from the server', async () => {
     const { container } = render(<Skills />)
 
     await waitFor(() => {
