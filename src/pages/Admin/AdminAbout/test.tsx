3cfb6fcefb263ecaa8351ef924e86433715f0ccc
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-16 10:34:45 -0300

    removed ckeditor

---
+++ src/pages/Admin/AdminAbout/test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import { AdminAbout } from '.'
+
+describe('ADMIN: About page', () => {
+  test('should render all fields', () => {
+    render(<AdminAbout />)
+
+    expect(screen.getByLabelText(/título/i)).toBeInTheDocument()
+    // expect(screen.getByLabelText(/Descrição/i)).toBeInTheDocument()
+    // expect(screen.getByLabelText(/habilidades/i)).toBeInTheDocument()
+    // expect(screen.getByRole('img')).toHaveAttribute('src', aboutPageMock.illustrationURL)
+    // expect(screen.getByRole('img')).toHaveAttribute('alt', aboutPageMock.illustrationURL)
+  })
+})
