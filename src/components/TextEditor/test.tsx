3c66866061d1d29cc6defa98b21da599495d9ebf
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-17 19:00:32 -0300

    add failed tests

--- src/components/TextEditor/test.tsx
+++ src/components/TextEditor/test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { describe, expect, test, vi } from 'vitest'
 import TextEditor from '.'
+import theme from '../../theme'
 
 describe('<TextEditor />', () => {
-  test('should render the component', async () => {
+  test('should render the component with default properties', async () => {
     const value = 'test editor'
     const onChange = vi.fn()
     render(
@@ -19,8 +21,42 @@ describe('<TextEditor />', () => {
     const editor = screen.getByText(/loading.../i)
 
     expect(label).toBeInTheDocument()
+    expect(label).toHaveStyle('color: #00021f')
     expect(wrapper).not.toHaveAttribute('disabled')
     expect(wrapper).not.toHaveAttribute('error')
     expect(editor).toBeInTheDocument()
   })
+
+  test('should render components with white label color', () => {
+    render(<TextEditor label="teste" labelColor="white" />)
+
+    const label = screen.getByText('teste')
+
+    expect(label).toBeInTheDocument()
+    expect(label).toHaveStyle('color: #FAFAFA')
+  })
+
+  test('should render components with error', () => {
+    const error = 'this is error'
+    render(<TextEditor label="teste" error={error} />)
+
+    const errorElement = screen.getByText(error)
+    expect(errorElement).toBeInTheDocument()
+    expect(errorElement).toHaveStyle({
+      color: theme.colors.red,
+      'font-size': theme.font.sizes.xsmall
+    })
+  })
+
+  test('should gain focus when tab', () => {
+    render(<TextEditor label="teste" initialValue="teste" />)
+
+    // const editor = screen.getByText(/loading.../i)
+
+    expect(document.body).toHaveFocus()
+    userEvent.tab()
+    // expect(editor).toHaveFocus()
+  })
+
+  test.todo('should focused when click on label')
 })
