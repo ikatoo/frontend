5bdb1772b1a3f36380cc0f351ca30dbb21022ed6
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-17 12:43:03 -0300

    add basic test for texteditor

--- src/components/TextEditor/test.tsx
+++ src/components/TextEditor/test.tsx
@@ -1,9 +1,26 @@
-import { render } from '@testing-library/react'
-import { describe, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { describe, expect, test, vi } from 'vitest'
 import TextEditor from '.'
 
 describe('<TextEditor />', () => {
-  test('should render the component', () => {
-    render(<TextEditor name="testEditor" placeholder="editor" />)
+  test('should render the component', async () => {
+    const value = 'test editor'
+    const onChange = vi.fn()
+    render(
+      <TextEditor
+        initialValue={value}
+        onChange={onChange}
+        label="Description"
+      />
+    )
+
+    const label = screen.getByText('Description')
+    const wrapper = label.parentElement
+    const editor = screen.getByText(/loading.../i)
+
+    expect(label).toBeInTheDocument()
+    expect(wrapper).not.toHaveAttribute('disabled')
+    expect(wrapper).not.toHaveAttribute('error')
+    expect(editor).toBeInTheDocument()
   })
 })
