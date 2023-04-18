2767fe4673b0002fb9f45814fe2dc13232d908c9
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-18 20:05:44 -0300

    removed monaco-editor

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,9 +1,9 @@
-import { Editor } from '@monaco-editor/react'
-import { useRef } from 'react'
+import TextArea from '../TextArea'
 import Styles from './styles'
 
 export type EditorProps = {
-  onChange?: (value: string) => void
+  onEditorChange?: (value: string) => void
+  name: string
   label?: string
   labelColor?: 'black' | 'white'
   initialValue?: string
@@ -13,37 +13,19 @@ export type EditorProps = {
 }
 
 const TextEditor = (props: EditorProps) => {
-  const editorRef = useRef<unknown>()
-
   const onChange = (value: string | undefined) => {
-    !!props.onChange && props.onChange(value ?? '')
-  }
-
-  const handleLabelClick = () => {
-    editorRef.current && (editorRef.current as { focus: () => void }).focus()
+    !!props.onEditorChange && props.onEditorChange(value ?? '')
   }
 
   return (
     <Styles.Wrapper disabled={props.disabled} error={!!props.error}>
-      {!!props.label && (
-        <Styles.Label labelColor={props.labelColor} onClick={handleLabelClick}>
-          {props.label}
-        </Styles.Label>
-      )}
       <Styles.EditorWrapper>
-        <Editor
-          height="200px"
-          defaultLanguage="html"
-          defaultValue={props.initialValue}
-          onChange={onChange}
-          onMount={(editor) => {
-            editorRef.current = editor
-          }}
-          options={{
-            tabIndex: props.tabIndex,
-            autoIndent: 'full',
-            formatOnPaste: true
-          }}
+        <TextArea
+          label={props.label}
+          labelColor={props.labelColor}
+          name={props.name}
+          tabIndex={1}
+          onTextAreaChange={onChange}
         />
       </Styles.EditorWrapper>
       {!!props.error && <Styles.Error>{props.error}</Styles.Error>}
