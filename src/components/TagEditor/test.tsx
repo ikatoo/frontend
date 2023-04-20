4c1163561dd47ed11cd34c6396b3140f373a2e05
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-20 00:23:47 -0300

    change async methods to sync

--- src/components/TagEditor/test.tsx
+++ src/components/TagEditor/test.tsx
@@ -4,7 +4,7 @@ import TagEditor from '.'
 import aboutPageMock from '../../mocks/aboutPageMock'
 
 describe('<TagEditor />', () => {
-  it('should render the component', async () => {
+  it('should render the component', () => {
     const title = 'Tags'
     render(
       <TagEditor name="tags" title={title} initalValue={aboutPageMock.skills} />
@@ -12,7 +12,7 @@ describe('<TagEditor />', () => {
 
     const inputElement = screen.getByRole('textbox')
     const tagsElement = screen.getByText(title)
-    const tagElements = await screen.findAllByTestId('tag-testid')
+    const tagElements = screen.queryAllByTestId('tag-testid')
 
     expect(inputElement).toBeInTheDocument()
     expect(tagsElement).toBeInTheDocument()
@@ -20,28 +20,28 @@ describe('<TagEditor />', () => {
     expect(tagElements).toHaveLength(aboutPageMock.skills.length)
   })
 
-  test('should remove tag when click in delete button', async () => {
+  test('should remove tag when click in delete button', () => {
     render(
       <TagEditor name="tags" title="Tags" initalValue={aboutPageMock.skills} />
     )
 
-    const tagForRemove = await screen.findByText('javascript')
-    const buttonElement = tagForRemove.lastElementChild
+    const tagForRemove = screen.queryByText('javascript')
+    const buttonElement = tagForRemove?.lastElementChild
 
     buttonElement && userEvent.click(buttonElement)
-    const tagElements = await screen.findAllByTestId('tag-testid')
+    const tagElements = screen.queryAllByTestId('tag-testid')
 
     expect(tagForRemove).not.toBeInTheDocument()
     expect(tagElements).toHaveLength(aboutPageMock.skills.length - 1)
   })
 
-  test('should add tag when key enter is pressed inner text input', async () => {
+  test('should add tag when key enter is pressed inner text input', () => {
     render(
       <TagEditor name="tags" title="Tags" initalValue={aboutPageMock.skills} />
     )
     const tagForAdd = 'new tag'
     const inputElement = screen.getByRole('textbox')
-    const tagElements = await screen.findAllByTestId('tag-testid')
+    const tagElements = screen.queryAllByTestId('tag-testid')
 
     expect(tagElements).toHaveLength(aboutPageMock.skills.length)
     userEvent.type(inputElement, tagForAdd)
@@ -51,7 +51,7 @@ describe('<TagEditor />', () => {
     expect(screen.queryByText('new tag')).toBeInTheDocument()
   })
 
-  test('should not add tag when this tag already exist', async () => {
+  test('should not add tag when this tag already exist', () => {
     const tags = [
       {
         title: 'new tag'
