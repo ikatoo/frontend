be3f8e1f5add2e0292cd86f5bea9c3975f348d8c
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-24 10:09:33 -0300

    :wheelchair: tab and focus order bettwen inputs

--- src/pages/Admin/AdminAbout/test.tsx
+++ src/pages/Admin/AdminAbout/test.tsx
@@ -1,12 +1,16 @@
 import { render, screen, waitFor } from '@testing-library/react'
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 import { AdminAbout } from '.'
 import aboutPageMock from '../../../mocks/aboutPageMock'
 import mockAboutService from '../../../mocks/msw/services/mockAboutService'
+import userEvent from '@testing-library/user-event'
 
 describe('ADMIN: About page', () => {
-  test('should render all fields', async () => {
+  beforeEach(() => {
     mockAboutService(200)
+  })
+
+  test('should render all fields', async () => {
     render(<AdminAbout />)
 
     expect(screen.getByLabelText(/título/i)).toBeInTheDocument()
@@ -36,5 +40,37 @@ describe('ADMIN: About page', () => {
           .map((skill) => ({ title: skill.textContent }))
       ).toEqual(skills)
     })
+  })
+
+  test('should change focus on press tab key', () => {
+    render(<AdminAbout />)
+
+    const titleInput = screen.getByRole('textbox', { name: /título/i })
+    const descriptionInput = screen.getByRole('textbox', { name: /Descrição/i })
+    const skillsInput = screen.getByRole('textbox', { name: /Habilidades/i })
+    const illustrationURLInput = screen.getByRole('textbox', {
+      name: /Imagem URL/i
+    })
+    const illustrationALTInput = screen.getByRole('textbox', {
+      name: /Imagem ALT/i
+    })
+    const submitButton = screen.getByRole('button', { name: /salvar/i })
+    const clearButton = screen.getByRole('button', {
+      name: /limpar formulário/i
+    })
+
+    expect(titleInput).toHaveFocus()
+    userEvent.tab()
+    expect(descriptionInput).toHaveFocus()
+    userEvent.tab()
+    expect(skillsInput).toHaveFocus()
+    userEvent.tab()
+    expect(illustrationURLInput).toHaveFocus()
+    userEvent.tab()
+    expect(illustrationALTInput).toHaveFocus()
+    userEvent.tab()
+    expect(submitButton).toHaveFocus()
+    userEvent.tab()
+    expect(clearButton).toHaveFocus()
   })
 })
