d4c1961c1c845cbdc6e726a0a386894e2e642c99
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-23 22:54:39 -0300

    add story with initial data

--- src/pages/Admin/AdminAbout/stories.tsx
+++ src/pages/Admin/AdminAbout/stories.tsx
@@ -5,12 +5,14 @@ import about from '../../../mocks/handlers/aboutHandler'
 export default {
   title: 'Pages/Privates/AdminAbout',
   component: AdminAbout,
-  decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>],
-  parameters: {
-    msw: {
-      handlers: about
-    }
-  }
+  decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>]
 } as ComponentMeta<typeof AdminAbout>
 
 export const Default = {} as ComponentStory<typeof AdminAbout>
+
+export const withData = {} as ComponentStory<typeof AdminAbout>
+withData.parameters = {
+  msw: {
+    handlers: about
+  }
+}
