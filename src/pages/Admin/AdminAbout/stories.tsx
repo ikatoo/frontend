32801c50ed840d1a00c19323ba5fc4a6ef61221f
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-24 18:33:02 -0300

    Revert "add msw handlers"
    This reverts commit 2e2be40f263e10b95653a32a3fc8e654fe7c6041.

--- src/pages/Admin/AdminAbout/stories.tsx
+++ src/pages/Admin/AdminAbout/stories.tsx
@@ -1,23 +1,11 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { AdminAbout } from '.'
-import env from '../../../helpers/env'
 import about from '../../../mocks/handlers/aboutHandler'
-import createHandlers from '../../../mocks/msw/services/createHandlers'
 
 export default {
   title: 'Pages/Privates/AdminAbout',
   component: AdminAbout,
-  decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>],
-  parameters: {
-    msw: {
-      handlers: [
-        createHandlers([
-          { url: `${env.VITE_API_URL}/about`, method: 'post', status: 201 },
-          { url: `${env.VITE_API_URL}/about`, method: 'patch', status: 204 }
-        ])
-      ]
-    }
-  }
+  decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>]
 } as ComponentMeta<typeof AdminAbout>
 
 export const Default = {} as ComponentStory<typeof AdminAbout>
