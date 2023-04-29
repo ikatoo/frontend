6a8c8a8eb4856c9bacf397223c78213aa74dbfdb
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-28 23:22:58 -0300

    ✅ (test): fill form data without use mouse

--- e2e/admin/about.spec.ts
+++ e2e/admin/about.spec.ts
@@ -1,18 +1,44 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+const _URL = '/admin/about'
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
-})
+test.describe('ADMIN - About page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(_URL)
+  })
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+  test('has page title', async ({ page }) => {
+    await expect(page).toHaveTitle(/ikatoo - software developer/i)
+  })
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click()
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/)
+  test('should call submit with data when save button is clicked', async ({
+    page
+  }) => {
+    await page.getByPlaceholder('Título').fill('Olá. Bem vindo❗')
+    await page.getByPlaceholder('Título').press('Tab')
+    await page
+      .getByPlaceholder('Descrição', { exact: true })
+      .fill(
+        '<p>Me chamo Milton Carlos Katoo, moro em Itapira, interior de São Paulo/Brasil. Pai de uma princesa e filho de excelente cozinheira Italiana e um saldoso Japonês faz tudo, sou um desenvolvedor full-stack que ama programação e desenvolvimento de software afim de melhorar a vida das pessoas.</p><p>Pessoa bem organizada, solucionador de problemas, funcionário independente com alta atenção aos detalhes. Gosto de animes, mangas, games, séries de TV e filmes. Pai orgulhoso de uma princesa, sou nascido em 1979 e sou innteressado em todo o espectro de programação.</p><a class="text-mck_aqua underline underline-offset-8" href="https://ikatoo.com.br/contact/" rel="contact"><span>🎉</span>Vamos fazer algo especial.</a><span>😄</span>'
+      )
+    await page.getByPlaceholder('Descrição', { exact: true }).press('Tab')
+    await page
+      .getByPlaceholder(
+        'Press "," | "Enter" | "Shift+Enter" to add Habilidades'
+      )
+      .fill('playwright')
+    await page
+      .getByPlaceholder(
+        'Press "," | "Enter" | "Shift+Enter" to add Habilidades'
+      )
+      .press('Tab')
+    await page
+      .getByPlaceholder('https://domain.com/image.jpg')
+      .fill('https://example.com.br/image.jpg')
+    await page.getByPlaceholder('https://domain.com/image.jpg').press('Tab')
+    await page
+      .getByPlaceholder('Uma breve descrição da imagem')
+      .fill('description for image')
+    await page.getByPlaceholder('Uma breve descrição da imagem').press('Enter')
+  })
 })
