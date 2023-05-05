b0e9f097f5fd2a37a1e66fb2adb211e127597ead
Milton Carlos Katoo <mckatoo@gmail.com>
2023-05-04 23:51:18 -0300

    verify if container exist and is up

--- docker/playwright/run.sh
+++ docker/playwright/run.sh
@@ -3,20 +3,32 @@
 SOURCE_PATH=$PWD
 SECCOMP_FILE_PATH=${SOURCE_PATH}/docker/playwright/seccomp_profile.json
 
-if [[ $(docker ps -a --filter name=playwright -q) ]]; then
-  echo "Container exists"
-  docker exec -t -i playwright \
-    bash docker/playwright/entry.sh
-else
-  echo "Creating a new container"
-  docker run -d \
-    --name=playwright \
+create_container ()
+{
+  docker run -t -i -d --name playwright \
+    -p 9323:9323 \
     -v .:/tests/ \
     -w /tests \
     --user pwuser \
-    --security-opt seccomp=${SECCOMP_FILE_PATH} \
-    --ipc=host \
-    mcr.microsoft.com/playwright:v1.33.0-jammy && \
-  docker exec -t -i playwright \
-    bash docker/playwright/entry.sh
+    mcr.microsoft.com/playwright:v1.33.0-jammy \
+    bash
+}
+
+run_in_container ()
+{
+  docker exec -t -i playwright bash docker/playwright/entry.sh
+}
+
+if [[ $(docker ps -a --filter name=playwright -q) ]]; then
+  if [[ $(docker ps --filter name=playwright -q) ]]; then
+    docker exec -t -i playwright \
+      bash docker/playwright/entry.sh
+  else
+    docker rm playwright
+    create_container
+    run_in_container
+  fi
+else
+  create_container
+  run_in_container
 fi
