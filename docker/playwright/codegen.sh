517e3dd06f7eb34f1297992a0b5f34e411795397
Milton Carlos Katoo <mckatoo@gmail.com>
2023-05-05 18:32:29 -0300

    abstract clear and create containers functions

--- docker/playwright/codegen.sh
+++ docker/playwright/codegen.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
-
-SOURCE_PATH=$PWD
-SECCOMP_FILE_PATH=${SOURCE_PATH}/docker/playwright/seccomp_profile.json
-
-clear_containers ()
-{
-  docker rm playwright
-}
-
-create_container ()
-{
-docker run -t -i -d \
-  --name playwright \
-  -p 9323:9323 \
-  -v .:/tests/ \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -e DISPLAY=$DISPLAY \
-  -w /tests \
-  --user pwuser \
-  mcr.microsoft.com/playwright:v1.33.0-jammy \
-  bash
-}
+source ./docker/playwright/base.sh
 
 run_in_container ()
 {
