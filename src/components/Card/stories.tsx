2eabda3ed0d673df98388d6d82cc2afd0d8f6728
Milton Carlos Katoo <mckatoo@gmail.com>
2023-05-11 20:46:34 -0300

    update story file

--- src/components/Card/stories.tsx
+++ src/components/Card/stories.tsx
@@ -1,16 +1,27 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react'
-
+import type { Meta, StoryObj } from '@storybook/react'
 import Card from '.'
-import projectsMock from '../../mocks/projectsMock'
+import projectsMock from 'src/mocks/projectsMock'
 
-export default {
+const meta: Meta<typeof Card> = {
   title: 'Components/Card',
-  component: Card,
-  args: {
-    ...projectsMock[1].description,
-    image: projectsMock[1].snapshot
-  }
-} as ComponentMeta<typeof Card>
+  component: Card
+}
 
-export const Default = {} as ComponentStory<typeof Card>
-Default.decorators = [(Story) => <div className="max-w-sm">{Story()}</div>]
+export default meta
+type Story = StoryObj<typeof Card>
+
+export const Default: Story = {
+  render: () => (
+    <div className="max-w-sm">
+      <Card {...projectsMock[1].description} />
+    </div>
+  )
+}
+
+export const With_Image: Story = {
+  render: () => (
+    <div className="max-w-sm">
+      <Card {...projectsMock[1].description} image={projectsMock[1].snapshot} />
+    </div>
+  )
+}
