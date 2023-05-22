091944297277daca136b4c46c5d210c122e99806
Milton Carlos Katoo <mckatoo@gmail.com>
2023-05-22 17:03:56 -0300

    external call open/close dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,13 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import * as Styles from './styles'
 
 export type DropdownProps = {
   title: React.ReactNode
+  handleIsOpen?: (isOpen: boolean) => void
+  initialIsOpenState?: boolean
   children: React.ReactNode
 } & Styles.ContentProps
 
-const Dropdown = ({ title, children, width }: DropdownProps) => {
-  const [isOpen, setIsOpen] = useState(false)
+const Dropdown = ({
+  title,
+  children,
+  width,
+  handleIsOpen,
+  initialIsOpenState = false
+}: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(initialIsOpenState)
+
+  useEffect(() => {
+    setIsOpen(initialIsOpenState)
+  }, [initialIsOpenState])
 
   useEffect(() => {
     document.body.style.overflow = isOpen ? 'hidden' : 'unset'
@@ -17,17 +29,18 @@ const Dropdown = ({ title, children, width }: DropdownProps) => {
     }
   }, [isOpen])
 
+  const onClick = () => {
+    !!(handleIsOpen && handleIsOpen(!isOpen)) || setIsOpen(!isOpen)
+  }
+
   return (
     <Styles.Wrapper isOpen={isOpen}>
-      <Styles.Title onClick={() => setIsOpen(!isOpen)}>{title}</Styles.Title>
+      <Styles.Title onClick={onClick}>{title}</Styles.Title>
 
       <Styles.Content width={width} aria-hidden={!isOpen}>
         {children}
       </Styles.Content>
-      <Styles.Overlay
-        aria-hidden={!isOpen}
-        onClick={() => setIsOpen(!isOpen)}
-      />
+      <Styles.Overlay aria-hidden={!isOpen} onClick={onClick} />
     </Styles.Wrapper>
   )
 }
--- src/components/Dropdown/stories.tsx
+++ src/components/Dropdown/stories.tsx
@@ -1,9 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { useState } from 'react'
 import Dropdown from '.'
 
 const meta: Meta<typeof Dropdown> = {
   title: 'components/Dropdown',
-  component: Dropdown
+  component: Dropdown,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
 }
 
 export default meta
@@ -11,4 +17,43 @@ type Story = StoryObj<typeof Dropdown>
 
 export const Default: Story = {
   render: () => <Dropdown title="Click here">Content</Dropdown>
+}
+
+export const External_Open: Story = {
+  render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+      <div>
+        <Dropdown
+          title="Click here"
+          handleIsOpen={setIsOpen}
+          initialIsOpenState={isOpen}
+        >
+          <span data-testid="content">
+            <button
+              type="button"
+              data-testid="button-to-close"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              Click to close
+            </button>
+          </span>
+        </Dropdown>
+
+        <button
+          className="absolute top-0 right-0"
+          type="button"
+          data-testid="button-to-open"
+        >
+          Click to open
+        </button>
+
+        <div className="absolute bottom-0 left-0">
+          {`isOpen state = ${isOpen}`}
+        </div>
+      </div>
+    )
+  }
 }
