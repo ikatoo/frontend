c027efa8fd5e6384033562e32553860ee4e7d745
Milton Carlos Katoo <mckatoo@gmail.com>
2023-05-25 12:49:58 -0300

    ✨ (feature): Add helper to transform string date formats

---
+++ src/helpers/date/test.ts
@@ -0,0 +1,17 @@
+import { dateToStringFormat, stringToDateFormat } from '.'
+
+describe('Date helper', () => {
+  test('should receive in string format and return to date format', () => {
+    const stringFormat = '2022 - 03'
+    const dateFormat = stringToDateFormat(stringFormat)
+
+    expect(dateFormat).toBe('03/2022')
+  })
+
+  test('should receive in date format and return to string format', () => {
+    const stringFormat = '03/2022'
+    const dateFormat = dateToStringFormat(stringFormat)
+
+    expect(dateFormat).toBe('2022 - 03')
+  })
+})
