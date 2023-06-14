b6c765aaa18a93a3e76c4dc0cb559da6739d765f
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-13 23:42:16 -0300

    return now when date is not informed

--- src/helpers/date/index.ts
+++ src/helpers/date/index.ts
@@ -2,7 +2,14 @@ const stringToDateFormat = (dateString: string) => {
   return dateString.split(' - ').reverse().toString().replaceAll(',', '/')
 }
 
-const dateToStringFormat = (date: string) => {
+const dateToStringFormat = (date: string | undefined) => {
+  if (!date) {
+    const newDate = new Date().toLocaleDateString(undefined, {
+      year: 'numeric',
+      month: 'numeric'
+    })
+    return newDate.split('/').reverse().toString().replaceAll(',', ' - ')
+  }
   return date.split('/').reverse().toString().replaceAll(',', ' - ')
 }
 
