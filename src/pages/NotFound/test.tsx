346f6e7885d27f30f138da534b4f17da9031016c
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-19 19:49:48 -0300

    added link to the home

--- src/pages/NotFound/test.tsx
+++ src/pages/NotFound/test.tsx
@@ -1,11 +1,28 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import { describe, expect, it } from 'vitest'
 import { NotFound } from '.'
 
 describe('Not Found Page', () => {
-  it('should render not found page when try access a bad route', () => {
-    const { getByText } = render(<NotFound />)
+  it('should render 404 page', () => {
+    render(
+      <MemoryRouter initialEntries={[`/`]}>
+        <NotFound />
+      </MemoryRouter>
+    )
 
-    expect(getByText(/page not found/i)).toBeInTheDocument()
+    expect(screen.getByText(/page not found/i)).toBeInTheDocument()
+  })
+
+  it('should have link go back to the home', () => {
+    render(
+      <MemoryRouter initialEntries={[`/`]}>
+        <NotFound />
+      </MemoryRouter>
+    )
+
+    expect(
+      screen.getByRole('link', { name: 'Go back to home.' })
+    ).toHaveAttribute('href', '/')
   })
 })
