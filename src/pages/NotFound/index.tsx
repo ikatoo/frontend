71d11b7f59dedac00c817b7a3334496af388498b
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-19 19:49:48 -0300

    added link to the home

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom'
 import Styles from './styles'
 
 export const NotFound = () => (
   <Styles.Wrapper>
     <Styles.Code>404</Styles.Code>
     <Styles.Message>Page not found</Styles.Message>
+    <Link to={'/'}>Go back to home.</Link>
   </Styles.Wrapper>
 )
--- src/pages/NotFound/stories.tsx
+++ src/pages/NotFound/stories.tsx
@@ -1,10 +1,21 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { MemoryRouter } from 'react-router-dom'
 import { NotFound } from '.'
 
-export default {
+const meta: Meta<typeof NotFound> = {
   title: 'Pages/NotFound',
-  component: NotFound,
-  decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>]
-} as ComponentMeta<typeof NotFound>
+  component: NotFound
+}
 
-export const Default = {} as ComponentStory<typeof NotFound>
+export default meta
+type Story = StoryObj<typeof NotFound>
+
+export const Default: Story = {
+  render: () => (
+    <MemoryRouter initialEntries={['/']}>
+      <div className="bg-mck_black_light">
+        <NotFound />
+      </div>
+    </MemoryRouter>
+  )
+}
