65d6faff9f43089e6c32b6d787c76068d7ecb8b6
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-20 12:53:01 -0300

    init projects page admin

---
+++ src/pages/Admin/AdminProjects/stories.tsx
@@ -0,0 +1,38 @@
+import env from 'src/helpers/env'
+import { AdminProjects } from '.'
+
+import type { Meta, StoryObj } from '@storybook/react'
+import { rest } from 'msw'
+import projectsPageMock from 'shared/mocks/projectsMock/result.json'
+
+const meta: Meta<typeof AdminProjects> = {
+  title: 'Pages/Privates/AdminProjects',
+  component: AdminProjects,
+  decorators: [(Story) => <div className="bg-mck_black_light">{Story()}</div>],
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(`${env.VITE_API_URL}/projects`, (_req, res, ctx) => {
+          return res(ctx.status(200), ctx.json({}))
+        })
+      ]
+    }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof AdminProjects>
+
+export const Default: Story = {}
+
+export const With_Data: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(`${env.VITE_API_URL}/projects`, (_req, res, ctx) => {
+          return res(ctx.status(200), ctx.json(projectsPageMock))
+        })
+      ]
+    }
+  }
+}
---
+++ src/pages/Admin/AdminProjects/styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+import tw from 'tailwind-styled-components'
+
+const Legend = styled.fieldset`
+  legend {
+    padding: 0 0.2rem;
+  }
+`
+
+export default {
+  Wrapper: tw.div`
+    flex
+    flex-col
+    md:flex-row
+    md:items-start
+    bg-mck_black_light
+    min-h-screen
+    min-w-screen
+
+    items-start
+    ml-4
+    md:mt-0
+    md:pt-4
+    mt-16
+    pt-4
+    pr-4
+  `,
+  TextWrapper: tw.div`
+    py-2
+    text-mck_gray_light
+  `,
+  Actions: tw.div`
+    flex
+    w-full
+    items-center
+    justify-center
+    m-4
+    gap-2
+  `,
+  Form: tw.form`
+    flex
+    flex-col
+    gap-2
+  `,
+  FieldSet: tw(Legend)`
+    border-solid
+    border-mck_gray_light
+    border-2
+    rounded
+    p-2
+  `
+}
