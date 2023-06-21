945c181fb53d4950f4135704adba2fa104202f17
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-21 16:49:33 -0300

    📝 (doc): Example for use the functions

--- src/helpers/date/index.ts
+++ src/helpers/date/index.ts
@@ -1,10 +1,16 @@
+/**
+ * Ex: '2022 - 03' -> '03/2022'
+ */
 const stringToDateFormat = (dateString: string) => {
   return dateString.split(' - ').reverse().toString().replaceAll(',', '/')
 }
 
+/**
+ * Ex: '03/2022' -> '2022 - 03'
+ */
 const dateToStringFormat = (date: string | undefined) => {
   if (!date || !date.length) return
   return date.split('/').reverse().toString().replaceAll(',', ' - ')
 }
 
-export { stringToDateFormat, dateToStringFormat }
+export { dateToStringFormat, stringToDateFormat }
