5d284298ce508407a98e9ace8ab53a3fcc3fce5b
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-23 19:57:25 -0300

    implementing drop file on upload component

--- src/components/UploadInput/index.tsx
+++ src/components/UploadInput/index.tsx
@@ -1,60 +1,50 @@
 import { useState } from 'react'
 import Button from '../Button'
-import TextInput, { TextInputProps } from '../TextInput'
 import Styles from './styles'
 
-export type UploadInputProps = Pick<
-  TextInputProps,
-  'name' | 'placeholder' | 'label' | 'labelColor'
-> & {
+export type UploadInputProps = {
+  name: string
+  placeholder: string
+  label: string
+  labelColor: string
   uploadFn?: () => void
 }
 
 const UploadInput = (props: UploadInputProps) => {
-  const [value, setValue] = useState('')
+  const [widthInput, setLarguraInput] = useState('')
+  const [heightInput, setAlturaInput] = useState('')
 
-  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    props.uploadFn && props.uploadFn()
+  const updateInput = (element: HTMLLabelElement) => {
+    if (element) {
+      const width = element.offsetWidth
+      const height = element.offsetHeight
+      setLarguraInput(`${width}px`)
+      setAlturaInput(`${height}px`)
+    }
   }
 
+  // const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   e.preventDefault()
+  //   props.uploadFn && props.uploadFn()
+  // }
+
+  // const uploadStyle: CSSProperties = {
+  //   width: labelRef.current?.width,
+  //   height: labelRef.current?.height
+  // }
+
   return (
-    <>
-      <Styles.Hidden>
-        <TextInput
-          type="file"
-          accept="image/*"
-          initialValue={value}
-          onInputChange={setValue}
-          name={props.name}
-          placeholder={props.placeholder}
-          label={props.label}
-          labelColor={props.labelColor}
-        />
-      </Styles.Hidden>
-      <Styles.FileArea htmlFor={props.name}>
-        <Styles.DropZone
-          onDrop={(event) => {
-            console.log(event)
-            event.preventDefault()
-          }}
-        >
-          {!value.length
-            ? 'Solte o arquivo aqui ou click para escolher'
-            : value}
-        </Styles.DropZone>
-      </Styles.FileArea>
-      <Styles.InlineButton>
-        <Button
-          styleType="secondary"
-          type="button"
-          onClick={onClick}
-          disabled={!value.length}
-        >
-          UPLOAD
-        </Button>
-      </Styles.InlineButton>
-    </>
+    <Styles.Container>
+      <Styles.UploadLabel ref={updateInput} htmlFor="file">
+        <Styles.Message>Drag & Drop to Upload</Styles.Message>
+        <Button styleType="primary">Choose a File</Button>
+      </Styles.UploadLabel>
+      <Styles.UploadHide
+        style={{ width: widthInput, height: heightInput }}
+        type="file"
+        id="file"
+      />
+    </Styles.Container>
   )
 }
 
