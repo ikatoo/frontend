7fbb3ba5278dce5d27c3ef7b09fb724d961274ec
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-26 00:18:01 -0300

    ✨ (feature): Upload field with drop area

--- src/components/UploadInput/index.tsx
+++ src/components/UploadInput/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import Button from '../Button'
 import Styles from './styles'
 
@@ -7,43 +7,72 @@ export type UploadInputProps = {
   placeholder: string
   label: string
   labelColor: string
+  onUploadChange?: (file: File) => void
   uploadFn?: () => void
 }
 
 const UploadInput = (props: UploadInputProps) => {
-  const [widthInput, setLarguraInput] = useState('')
-  const [heightInput, setAlturaInput] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  const updateInput = (element: HTMLLabelElement) => {
-    if (element) {
-      const width = element.offsetWidth
-      const height = element.offsetHeight
-      setLarguraInput(`${width}px`)
-      setAlturaInput(`${height}px`)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    !!error.length && Promise.resolve(setTimeout(() => setError(''), 5000))
+  }, [error.length])
+
+  const onInputChange = (item: File) => {
+    if (!item) return
+    if (!item?.type.startsWith('image/')) {
+      return setError('Image only.')
     }
+    props.onUploadChange && props.onUploadChange(item)
   }
 
-  // const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault()
-  //   props.uploadFn && props.uploadFn()
-  // }
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault()
+    const { files } = event.dataTransfer
+    const item = files.item(0)
+    !!item && onInputChange(item)
+  }
 
-  // const uploadStyle: CSSProperties = {
-  //   width: labelRef.current?.width,
-  //   height: labelRef.current?.height
-  // }
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault()
+  }
+
+  const handleDropAreaClick = () => {
+    const inputElement = inputRef.current
+    inputElement && inputElement.click()
+  }
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    const inputElement = inputRef.current
+    const item = inputElement?.files?.item(0)
+    !!item && onInputChange(item)
+  }
 
   return (
     <Styles.Container>
-      <Styles.UploadLabel ref={updateInput} htmlFor="file">
-        <Styles.Message>Drag & Drop to Upload</Styles.Message>
-        <Button styleType="primary">Choose a File</Button>
-      </Styles.UploadLabel>
-      <Styles.UploadHide
-        style={{ width: widthInput, height: heightInput }}
+      <input
+        hidden
+        ref={inputRef}
         type="file"
+        name="file"
         id="file"
+        accept="image/*"
+        onChange={handleInputChange}
       />
+      <Styles.DropArea
+        onClick={handleDropAreaClick}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+      >
+        <Styles.Error>{error}</Styles.Error>
+        {!error.length && <span>{props.label}</span>}
+      </Styles.DropArea>
+      <Button styleType="primary" onClick={props.uploadFn}>
+        UPLOAD
+      </Button>
     </Styles.Container>
   )
 }
