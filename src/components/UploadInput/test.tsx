df5440b9ad5134dbd739243f495bd69a2bb739fe
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-27 11:55:50 -0300

    inital tests to upload-component

---
+++ src/components/UploadInput/test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import UploadInput from '.'
+
+describe('<UploadInput />', () => {
+  it('should render the component', () => {
+    render(<UploadInput name="test" label="file here" />)
+    const component = screen.getByText('file here')
+
+    expect(component).toBeInTheDocument()
+  })
+
+  it.todo('should render a disabled component')
+  it.todo('should disabled upload button while input file is empty')
+  it.todo('should update label with name and size of the choosed file')
+  it.todo('should call function when choose file')
+  it.todo('should call function when press upload button')
+  it.todo(
+    'should the label show an error message when the chosen file is not an image'
+  )
+  it.todo('should enable upload button when file is choose')
+})
