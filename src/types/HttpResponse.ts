9a76c58cc7d8bbaa5e3392b0fbd29734617b255f
Milton Carlos Katoo <mckatoo@gmail.com>
2023-08-27 20:04:43 -0300

    validate response of the api

---
+++ src/types/HttpResponse.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const HttpResponseSchema = z.object({
+  data: z
+    .object({
+      message: z.string().optional()
+    })
+    .or(z.any()),
+  status: z.number().int().positive()
+})
+
+export const PartialHttpResponseSchema = HttpResponseSchema.partial()
+
+export type HttpResponse = z.infer<typeof HttpResponseSchema>
