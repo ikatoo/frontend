f9a87f823270219eb79071a63078f0d758e9e88d
Milton Carlos Katoo <mckatoo@gmail.com>
2023-12-19 20:57:09 -0300

    refactored skills page and service

--- src/services/skillsService/index.ts
+++ src/services/skillsService/index.ts
@@ -3,22 +3,24 @@ import api from '../api'
 
 export default {
   create: async (pageData: Partial<SkillsPageProps>) => {
-    const { data, status } = await api.post('skills', pageData)
+    const { data, status } = await api.post('skills-page', pageData)
     const json =
       typeof data === 'string' && data !== '' ? JSON.parse(data) : data
 
     return { data: json, status }
   },
   patch: async (pageData: Partial<SkillsPageProps>) => {
-    const { data, status } = await api.patch('skills', pageData)
+    const { data, status } = await api.patch('skills-page', pageData)
     const json =
       typeof data === 'string' && data !== '' ? JSON.parse(data) : data
 
     return { data: json, status }
   },
-  get: async () => {
+  get: async (userId = 1) => {
     try {
-      const { data, status } = await api.get<SkillsPageProps>('skills')
+      const { data, status } = await api.get<SkillsPageProps>(
+        `skills-page/user-id/${userId}`
+      )
       const json: SkillsPageProps =
         typeof data === 'string' && data !== '' ? JSON.parse(data) : data
 
