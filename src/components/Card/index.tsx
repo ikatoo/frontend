d9e6f9a1e85d21ee8e84b1885acc52f0bbe1e6d7
Milton Carlos Katoo <mckatoo@gmail.com>
2023-12-30 19:29:57 -0300

    removed stretch and add tags properties

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { Tag } from '../TagEditor'
 import Styles from './styles'
 
 export type CardProps = {
@@ -6,29 +7,34 @@ export type CardProps = {
   subTitle?: string
   content: string
   link?: string
-  stretch?: boolean
+  tags?: Tag[]
 }
 
-const Card = ({
-  stretch = false,
-  image,
-  title,
-  subTitle,
-  content,
-  link
-}: CardProps) => {
+const Card = ({ image, title, subTitle, content, link, tags }: CardProps) => {
   const Content = () => (
     <>
       {!!image && <Styles.Image src={image} alt={title} />}
-      <Styles.ContentWrapper>
-        <Styles.Title>{title}</Styles.Title>
-        <Styles.Subtitle>{subTitle}</Styles.Subtitle>
-        <Styles.Content>{content}</Styles.Content>
-      </Styles.ContentWrapper>
+      <Styles.BottomWrapper>
+        <Styles.InfoWrapper>
+          <Styles.Title>{title}</Styles.Title>
+          <Styles.Description>
+            <Styles.Subtitle>{subTitle}</Styles.Subtitle>
+            <Styles.Content>{content}</Styles.Content>
+          </Styles.Description>
+        </Styles.InfoWrapper>
+
+        {tags && (
+          <Styles.Tags>
+            {tags.map((tag, index) => (
+              <Styles.Tag key={index}>{tag.title}</Styles.Tag>
+            ))}
+          </Styles.Tags>
+        )}
+      </Styles.BottomWrapper>
     </>
   )
   return (
-    <Styles.Wrapper $stretch={stretch}>
+    <Styles.Wrapper>
       {link ? (
         <a href={link} target="_blank" rel="noreferrer">
           <Content />
