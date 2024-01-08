7adcf5305757b70ff6d02864aac11d97d3f52544
Milton Carlos Katoo <mckatoo@gmail.com>
2024-01-08 02:17:32 -0300

    ♻️ (refactor): Add fields for send email

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from 'react'
+import Form from 'src/components/Form'
+import { FormContainer } from 'src/components/FormContainer'
+import TextInput from 'src/components/TextInput'
+import TextWrapper from 'src/components/TextWrapper'
 import setPageSubtitle from 'src/helpers/setPageSubtitle'
 import CommonWrapper from 'src/styles/common/wrapper'
 import { LocalizationType } from 'src/types/LocalizationType'
@@ -6,11 +10,13 @@ import Map from '../../components/Map'
 import { TextContainer } from '../../components/TextContainer'
 import contactService from '../../services/contactService'
 import Styles from './styles'
+import TextArea from 'src/components/TextArea'
 
 export const Contact = () => {
   const [description, setDescription] = useState('')
   const [title, setTitle] = useState('')
   const [localization, setLocalization] = useState<LocalizationType>()
+  const [focused, setFocused] = useState(true)
 
   useEffect(() => {
     setPageSubtitle('Contact Page')
@@ -25,24 +31,62 @@ export const Contact = () => {
     getInitialData()
   }, [])
 
+  const onReset = () => {
+    setFocused(true)
+  }
+
   return (
     <CommonWrapper>
       <Styles.Description>
         {
           <TextContainer title={title}>
             <div dangerouslySetInnerHTML={{ __html: description }} />
+
+            <FormContainer>
+              <Form onReset={onReset} method="post" name="contactPageForm">
+                <TextWrapper>
+                  <TextInput
+                    focus={focused}
+                    onBlur={() => {
+                      setFocused(false)
+                    }}
+                    labelColor="white"
+                    label="Nome"
+                    name="name"
+                    placeholder="Seu Nome"
+                    autoFocus={focused}
+                  />
+                </TextWrapper>
+
+                <TextWrapper>
+                  <TextInput
+                    labelColor="white"
+                    label="Email"
+                    name="email"
+                    placeholder="seu@email.com"
+                  />
+                </TextWrapper>
+
+                <TextWrapper>
+                  <TextArea
+                    label="Mensagem"
+                    name="message"
+                    placeholder="Escreva sua mensagem..."
+                  />
+                </TextWrapper>
+              </Form>
+            </FormContainer>
           </TextContainer>
         }
       </Styles.Description>
 
-      <Styles.MapWrapper data-testid="map-testid">
-        <Map center={localization} />
-      </Styles.MapWrapper>
-      {/* {!!localization && (
-        <Styles.MapWrapper data-testid="map-testid">
-          <Map center={localization} />
-        </Styles.MapWrapper>
-      )} */}
+      {!!localization && (
+        <Styles.AditionalContent>
+          <Styles.MapWrapper data-testid="map-testid">
+            <Map center={localization} />
+          </Styles.MapWrapper>
+        </Styles.AditionalContent>
+      )}
     </CommonWrapper>
   )
 }
