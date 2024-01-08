370a9ad8ee806fc96af544812b2019228297e997
Milton Carlos Katoo <mckatoo@gmail.com>
2024-01-08 02:17:32 -0300

    ♻️ (refactor): Add fields for send email

--- src/pages/Contact/styles.ts
+++ src/pages/Contact/styles.ts
@@ -1,19 +1,31 @@
-import tw from 'tailwind-styled-components'
+import styled from 'styled-components'
 
 export default {
-  Description: tw.div`
-    w-full
+  Description: styled.div`
+    width: 100%;
+    height: 100vh;
   `,
-  MapWrapper: tw.div`
-    rounded
-    overflow-auto
-    aspect-square
-    w-full
-    z-10
-    p-24
-    -my-16
+  AditionalContent: styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  MapWrapper: styled.div`
+    border-radius: 0.25rem;
+    overflow: auto;
+    margin-top: -4rem;
+    margin-bottom: -4rem;
+    width: 16rem;
+    height: 16rem;
+    z-index: 0;
 
-    md:p-0
-    md:m-0
+    @media (min-width: 768px) {
+      padding: 0px;
+      margin: 0px;
+      display: flex;
+      justify-content: center;
+    }
   `
 }
