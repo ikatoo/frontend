92c22f3f335e4e63d360b37018c0e18fad833147
Milton Carlos Katoo <mckatoo@gmail.com>
2024-02-15 20:08:39 -0300

    removed playwright and add selenium with webdriver

--- e2e/about.spec.ts
+++ e2e/about.spec.ts
@@ -1,16 +1,10 @@
-import { expect, test } from '@playwright/test'
-import aboutPageMock from 'shared/mocks/aboutPageMock/result.json' assert { type: 'json' }
-
 const _URL = '/'
 
-test.describe('About page', () => {
-  test('should show about page on root path', async ({ page }) => {
-    await page.route(`${process.env.VITE_API_URL}/about`, (route) => {
-      route.fulfill({ status: 200, json: aboutPageMock })
-    })
-    await page.goto(_URL)
+describe('About page', () => {
+  it('should show about page on root path', async () => {
+    await browser.navigateTo(_URL)
 
-    await expect(page).toHaveTitle(/ikatoo - software developer/i)
-    await expect(page).toHaveURL('/about')
+    await expect(browser).toHaveTitle(/ikatoo - software developer/i)
+    await expect(browser).toHaveURL('/about')
   })
 })
--- e2e/admin/skills.spec.ts
+++ e2e/admin/skills.spec.ts
@@ -1,9 +1,12 @@
-import { expect, test } from '@playwright/test'
-import axios from 'axios'
+import { expect, request, test } from '@playwright/test'
 import { randomBytes } from 'crypto'
 import { authorize } from 'src/helpers/playwrightUtils'
 
 const _URL = '/admin/skills'
+
+const api = await request.newContext({
+  baseURL: process.env.VITE_API_URL
+})
 
 test.describe('ADMIN - Skills page', () => {
   test('has page title', async ({ page }) => {
@@ -25,8 +28,11 @@ test.describe('ADMIN - Skills page', () => {
     const accessToken = `${
       (await context.storageState()).origins[0].localStorage[0].value
     }`
-    axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`
-    axios.delete(process.env.VITE_API_URL + '/skills-page')
+    await api.delete('/skills-page', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
     await page.reload({ waitUntil: 'networkidle' })
 
     await page.getByLabel('Título').fill('new title' + randomTestId)
@@ -38,7 +44,7 @@ test.describe('ADMIN - Skills page', () => {
     await expect(page.getByText('Success on create skills page.')).toBeVisible()
   })
 
-  test('should complete update skills page data', async ({ page }) => {
+  test('should complete update skills page data', async ({ page, context }) => {
     const randomTestId = randomBytes(10).toString('hex')
     const newData = {
       title: 'new title' + randomTestId,
@@ -47,19 +53,36 @@ test.describe('ADMIN - Skills page', () => {
 
     await page.goto(_URL)
     await authorize(page)
-    await page.waitForLoadState()
+
+    await page.waitForURL('http://localhost:3000/admin/skills')
+    const accessToken = `${
+      (await context.storageState()).origins[0].localStorage[0].value
+    }`
+    const data = await (await api.get('skills-page/user-id/1')).json()
+    if (!data?.title)
+      await api.post('/skills-page', {
+        data: {
+          title: 'first title',
+          description: 'first description'
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+    await page.reload({ waitUntil: 'networkidle' })
 
     const title = page.getByLabel('Título')
     const description = page.getByPlaceholder('Descrição', { exact: true })
 
     await title.fill(newData.title)
     await description.fill(newData.description)
+
     await page.getByRole('button', { name: 'Atualizar' }).click()
 
     await expect(page.getByText('Success on update skills page.')).toBeVisible()
   })
 
-  test('should partial update skills page data', async ({ page }) => {
+  test('should partial update skills page data', async ({ page, context }) => {
     const randomTestId = randomBytes(10).toString('hex')
     const newData = {
       title: 'new title' + randomTestId,
@@ -68,7 +91,23 @@ test.describe('ADMIN - Skills page', () => {
 
     await page.goto(_URL)
     await authorize(page)
-    await page.waitForLoadState()
+
+    await page.waitForURL('http://localhost:3000/admin/skills')
+    const accessToken = `${
+      (await context.storageState()).origins[0].localStorage[0].value
+    }`
+    const data = await (await api.get('skills-page/user-id/1')).json()
+    if (!data?.title)
+      await api.post('/skills-page', {
+        data: {
+          title: 'first title',
+          description: 'first description'
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+    await page.reload({ waitUntil: 'networkidle' })
 
     const title = page.getByLabel('Título')
 
