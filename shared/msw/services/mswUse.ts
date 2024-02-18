ec1ff2d411430787994c3cf5c5ae8a6ed4485beb
Milton Carlos Katoo <mckatoo@gmail.com>
2024-02-17 21:17:20 -0300

    removed playwright relationed files

--- shared/msw/services/mswUse.ts
+++
@@ -1,8 +0,0 @@
-import { mswServer } from '../mswServer'
-import createHandlers, { HandlerProps } from './createHandlers'
-
-export default (props: HandlerProps[]) => {
-  const handlers = createHandlers(props)
-
-  mswServer.use(...handlers)
-}
