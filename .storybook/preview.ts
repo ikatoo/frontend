e22ebb03c2bf01b6d3cc992be745039b0b18f8c4
Milton Carlos Katoo <mckatoo@gmail.com>
2024-03-05 10:33:36 -0300

    removed msw-storybook-addon

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,6 @@
 import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 import { themes } from "@storybook/theming";
 import '../src/index.css';
-import { initialize, mswLoader } from 'msw-storybook-addon';
-
-initialize();
-
-export const loaders = [mswLoader]
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
